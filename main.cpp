248f5d1df829a3856810b2458fa92d67f771328a
catbron <catbaron@live.cn>
2014-12-01 15:59:14 +0900

    Update main.cpp
    1. work flow连好。输入脏图片，切成小块，拿出来做分类。
    2. 增加宏定义，方便更换不同的getdescriptor函数。

--- main.cpp
+++ main.cpp
@@ -59,11 +59,48 @@
 using namespace cv;
 using namespace std;
 
+
+
+const int NIBLACK_STEP = 3;
+const float VARIANCE_THRESHOLD = 5.0;
+
+// Choose the getdescriptor function
+#define _33_
+#ifdef _ZG1_
+#define getDescriptorFromImage _zg1_getDescriptorFromImage
+const int MAX_DIM_DESCRIPTOR = 257;
+const int STEP = 1;
+#endif // _ZG1_
+#ifdef _ZG2_
+#define getDescriptorFromImage _zg2_getDescriptorFromImage
+const int MAX_DIM_DESCRIPTOR = 96;
+const int STEP = 16;
+
+#endif // _ZG1_
+#ifdef _33_
+const int STEP = 1;
+const int MAX_DIM_DESCRIPTOR = 120;
+#define getDescriptorFromImage _33_getDescriptorFromImage
+#endif // _ZG1_
+
+const double threshold_box_size_min01 = 0.0001; // approximately 15 x 15 pixel for 1600 x 1200 image 
+const double threshold_box_size_max01 = 0.01;  // approximately 150 x 150 pixel for 1600 x 1200 image
+const double threshold_box_size_min02 = 0.0001; // approximately 15 x 15 pixel for 1600 x 1200 image 
+const double threshold_box_size_max02 = 0.01;  // approximately 150 x 150 pixel for 1600 x 1200 image
+const double threshold_similarity = 0.4;
+const double threshold_length_connected_box_min = 0.005; // approximately 100 pixel for 1600 x 1200 image
+const double threshold_length_connected_box_max = 0.5; // approximately 1000 pixel for 1600 x 1200 image
+const double threshold_aspect_ratio = 0.2;
+const double threshold_relative_distance = 1.0;
+const int threshold_count_label = 7;
+const int num_bilateral_filtering01 = 12;
+const int num_bilateral_filtering02 = 3;
+const double threshold_score = 0.5;
+
 const int NUM_POINTS = 16;
-const int MAX_DIM_DESCRIPTOR = 256;
+
 const int THRESHOLD_NEIGHBORHOOD = 5.0;
 const int SAMPLE_POINT_P = 1;
-const int STEP = 16;
 const int SAMPLE_POINT_Q = 9;
 const int FILE_NAME_LENGTH = 100;
 typedef char Cfname[FILE_NAME_LENGTH];
@@ -71,6 +108,8 @@ typedef float Dcount[MAX_DIM_DESCRIPTOR];
 const int CLASS_NUM = 62;
 const int TEST_LOC = CLASS_NUM;
 const int HULL_LIST_MAX = 20;
+
+
 float classLabels[CLASS_NUM];
 char classDir[CLASS_NUM + 1][10] = {
 	"A0", "B0", "C0", "D0",
@@ -91,7 +130,295 @@ char classDir[CLASS_NUM + 1][10] = {
 	"8", "9", "TEST"
 };
 char testDir[] = "TEST";
-int getDescriptorFromImage(char* imageFile, float* descriptor)
+
+Mat niBlack(Mat input_img)
+{
+	///////// Niblack + findContours /////////
+
+	cv::Mat Gray_img;
+	cv::Mat Niblack_img01 = cv::Mat::zeros(input_img.rows, input_img.cols, CV_8UC1);
+	cv::Mat Niblack_img02 = cv::Mat::zeros(input_img.rows, input_img.cols, CV_8UC1);
+
+	//cv::Mat canny_img01;
+
+	cv::cvtColor(input_img, Gray_img, CV_BGR2GRAY);
+
+	//cv::Canny(bilateral_img01, canny_img01, 50, 250, 5, false);
+
+	double average = 0.0, threshold, variance = 0.0, n = 0.0;
+
+	int count000 = 0;
+	int count255 = 0;
+
+	for (int j = 0; j<Gray_img.rows; ++j)
+	{
+		for (int i = 0; i<Gray_img.cols; ++i)
+		{
+			n = 0.0;
+
+			for (int l = j - NIBLACK_STEP; l <= j + NIBLACK_STEP; ++l)
+			{
+				if ((l >= 0) && (l<Gray_img.rows))
+				{
+					for (int k = i - NIBLACK_STEP; k <= i + NIBLACK_STEP; ++k)
+					{
+						if (k >= 0 && k<Gray_img.cols)
+						{
+							average += (double)Gray_img.at<uchar>(l, k);
+						}
+
+						n = n + 1.0;
+						//std::cout << " n = " << n << std::endl;
+					}
+				}
+			}
+
+			average = average / n;
+
+			n = 0.0;
+
+			for (int l = j - NIBLACK_STEP; l <= j + NIBLACK_STEP; ++l)
+			{
+				if (l >= 0 && l<Gray_img.rows)
+				{
+					for (int k = i - NIBLACK_STEP; k <= i + NIBLACK_STEP; ++k)
+					{
+						if (k >= 0 && k<Gray_img.cols)
+						{
+							variance += ((double)Gray_img.at<uchar>(l, k) - average)*((double)Gray_img.at<uchar>(l, k) - average);
+						}
+
+						n = n + 1.0;
+					}
+				}
+			}
+
+			variance = variance / n;
+			variance = sqrt(variance);
+
+			threshold = average + (-0.1)*variance;
+
+			//std::cout << " average = " << average << "  variance = " << variance << "   threshold = " << threshold << std::endl;
+
+
+			//std::cout << " Gray_img.at<uchar>(j,i) = " << (int)Gray_img.at<uchar>(j,i) << std::endl;
+			//generate Niblack_img01
+			if ((Gray_img.at<uchar>(j, i) > threshold) && (variance >= VARIANCE_THRESHOLD))
+			{
+				Niblack_img01.at<uchar>(j, i) = 0;
+				count000++;
+			}
+			else
+			{
+				Niblack_img01.at<uchar>(j, i) = 255;
+				count255++;
+			}
+
+			//generate Niblack_img02
+			if ((Gray_img.at<uchar>(j, i) < threshold) && (variance >= VARIANCE_THRESHOLD))
+			{
+				Niblack_img02.at<uchar>(j, i) = 0;
+				count000++;
+			}
+			else
+			{
+				Niblack_img02.at<uchar>(j, i) = 255;
+				count255++;
+			}
+			average = 0.0;
+			variance = 0.0;
+		}
+	}
+
+	//std::cout << " count000 = " << count000 << " count255 = " << count255 << std::endl;
+	
+	//if (flag_display_canny_img == 1)
+	//{
+	//	cv::namedWindow("Niblack_img01", 1);
+	//	cv::imshow("Niblack_img01", Niblack_img01);
+	//	cv::namedWindow("Niblack_img02", 1);
+	//	cv::imshow("Niblack_img02", Niblack_img02);
+	//	cv::namedWindow("canny_img01", 1);
+	//	cv::imshow("canny_img01", canny_img01);
+	//}
+	return Niblack_img02;
+}
+
+int getTextBox(char* imageFile)
+{
+	cv::Mat input_img = cv::imread(imageFile, 1);
+	if (!input_img.data) {
+		cout << "open image file:" << imageFile << " failed!" << endl;
+		return -1;
+	}
+
+	cv::namedWindow("input_img", 1);
+	cv::imshow("input_img", input_img);
+
+	//bilateral filter
+	double size_total_pixels = input_img.rows*input_img.cols;
+	cv::Mat bilateral_img01; // In cv::bilateralFilter(), output_array must be different frome input_array. So we prepared bilateral_img0101 for input_array.
+	cv::Mat bilateral_img02; // We prepared bilateral_img0102 for output_array.
+
+	bilateral_img01 = input_img.clone();
+
+	for (int i = 0; i < num_bilateral_filtering01; ++i)
+	{
+		cv::bilateralFilter(bilateral_img01, bilateral_img02, 5, 40, 200, 4);
+		bilateral_img01 = bilateral_img02.clone();
+		bilateral_img02 = cv::Mat::zeros(input_img.cols, input_img.rows, CV_8UC3);
+	}
+
+	///////// canny + findContours /////////
+
+	cv::Mat canny_img;
+
+	cv::cvtColor(bilateral_img01, canny_img, CV_BGR2GRAY);
+#ifdef _ZG1_
+	Mat niblack_img = input_img.clone();
+	niblack_img = niBlack(niblack_img);
+	//test NiBlack
+	cvNamedWindow("niblack");
+	imshow("niblack", niblack_img);
+#endif // _ZG1_
+	cv::Canny(canny_img, canny_img, 50, 250, 5, false);
+
+
+	std::vector<std::vector<cv::Point>> contours01;
+	std::vector<cv::Vec4i> hierarchy01;
+
+	cv::findContours(canny_img, contours01, hierarchy01, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+	std::vector<cv::Rect> list_box01(contours01.size());
+
+	for (unsigned int i = 0; i < contours01.size(); ++i)
+	{
+		list_box01[i] = cv::boundingRect(cv::Mat(contours01[i]));
+	}
+
+	// display contour boxes
+	cv::Scalar color = cv::Scalar(255, 0, 0);
+	cv::Mat cc_img00 = input_img.clone();
+
+	for (unsigned int j = 0; j < contours01.size(); ++j)
+	{
+		cv::rectangle(cc_img00, list_box01[j].tl(), list_box01[j].br(), color, 2, 8, 0);  // tl = top left point, br = bottom right point
+	}
+	
+	///////// Basic rules for wiping out useless boxes /////////
+
+	///// filtering01 : filtering by the size (too large or too small) of boxes
+
+	// make filter01
+
+	double current_box_size;
+	std::vector<cv::Rect> list_box_filter01(contours01.size());
+	int f01 = 0;
+
+	for (unsigned int i = 0; i<contours01.size(); ++i)
+	{
+		current_box_size = (double)list_box01[i].width * list_box01[i].height;
+
+		if ((current_box_size > threshold_box_size_min01*size_total_pixels) && (current_box_size < threshold_box_size_max01*size_total_pixels))
+		{
+			list_box_filter01[f01].tl() = list_box01[i].tl();
+			list_box_filter01[f01].br() = list_box01[i].br();
+			list_box_filter01[f01].width = list_box01[i].width;
+			list_box_filter01[f01].height = list_box01[i].height;
+			list_box_filter01[f01].x = list_box01[i].x;
+			list_box_filter01[f01].y = list_box01[i].y;
+			f01++;
+		}
+	}
+
+	//std::cout << "f01 = " << f01 << std::endl;
+
+	// display filtering01 image
+
+	cv::Mat cc_img01 = input_img.clone();
+
+	for (int i = 0; i<f01; ++i)
+	{
+		cv::rectangle(cc_img01, list_box_filter01[i].tl(), list_box_filter01[i].br(), cv::Scalar(255, 0, 0), 2, 8, 0);  // tl = top left point, br = bottom right point 
+	}
+
+
+	///// filtering02 : filtering by aspect ration of the box
+
+	// make filter02
+
+	float hw, wh, current_aspect_ratio;
+	std::vector<cv::Rect> list_box_filter02(contours01.size());
+	int f02 = 0;
+
+	for (int i = 0; i<f01; ++i)
+	{
+		hw = (float)list_box_filter01[i].height / list_box_filter01[i].width;
+		wh = (float)list_box_filter01[i].width / list_box_filter01[i].height;
+		if (hw >= wh)
+			current_aspect_ratio = wh;
+		else
+			current_aspect_ratio = hw;
+
+		if (current_aspect_ratio > threshold_aspect_ratio)
+		{
+			list_box_filter02[f02].tl() = list_box_filter01[i].tl();
+			list_box_filter02[f02].br() = list_box_filter01[i].br();
+			list_box_filter02[f02].width = list_box_filter01[i].width;
+			list_box_filter02[f02].height = list_box_filter01[i].height;
+			list_box_filter02[f02].x = list_box_filter01[i].x;
+			list_box_filter02[f02].y = list_box_filter01[i].y;
+			f02++;
+		}
+	}
+
+	//std::cout << "f02 = " << f02 << std::endl;
+
+	// display filtering02 image
+
+	cv::Mat cc_img02 = input_img.clone();
+
+	for (int i = 0; i<f02; ++i)
+	{
+		//cv::drawContours(cc_img_filter02, approx_curve, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+		cv::rectangle(cc_img02, list_box_filter02[i].tl(), list_box_filter02[i].br(), cv::Scalar(255, 0, 0), 2, 8, 0);  // tl = top left point, br = bottom right point 
+	}
+
+	///////// Basic rules for wiping out useless boxes /////////
+
+	///// filtering01 : filtering by the size (too large or too small) of boxes
+
+	// make filter01
+	Mat img_out_test;
+	for (int i = 0; i < f02; i++)
+	{
+		char fname[50];
+		sprintf_s(fname, "TEST/%d_%s", i, imageFile);
+		cout << "out put " << fname << endl;
+		int x1, y1, w, h;
+		x1 = list_box_filter02[i].tl().x;
+		y1 = list_box_filter02[i].tl().y;
+		w = list_box_filter02[i].width;
+		h = list_box_filter02[i].height;
+		Rect rect(x1, y1, w, h);
+#ifdef DEBUG
+		img_out_test = niblack_img(rect);
+#endif // DEBUG
+
+		img_out_test = input_img(rect);
+		imwrite(fname,img_out_test);
+	}
+	//cv::namedWindow("canny_img", 1);
+	//cv::imshow("canny_img", canny_img);
+	//cv::namedWindow("cc_img00", 1);
+	//cv::imshow("cc_img00", cc_img00);
+	//cv::namedWindow("cc_img01", 1);
+	//cv::imshow("cc_img01", cc_img01);
+	//cv::namedWindow("cc_img02", 1);
+	//cv::imshow("cc_img02", cc_img02);
+}
+
+int _zg1_getDescriptorFromImage(char* imageFile, float* descriptor)
 {
 	cv::Mat input_img = cv::imread(imageFile, 1);
 	if (!input_img.data) {
@@ -111,10 +438,19 @@ int getDescriptorFromImage(char* imageFile, float* descriptor)
 	cv::cvtColor(resized_img, gray_img, CV_BGR2GRAY);
 	//// Generate Gray end
 
+	// Test Niblack
+	//cvNamedWindow("Niblack");
+	//imshow("Niblack", resized_img);
+	//waitKey(0);
+	
+	
 	////weight points////
 
-	Mat w_map(resized_img.cols, resized_img.rows, CV_32FC1);
+	int c = resized_img.cols;
+	int r = resized_img.rows;
+	Mat w_map(c, r, CV_32FC1);
 	int w_cn = 0;
+	int ff = 0;
 	for (int i = 0; i < resized_img.cols; ++i)
 	{
 		for (int j = 0; j < resized_img.rows; ++j)
@@ -122,6 +458,7 @@ int getDescriptorFromImage(char* imageFile, float* descriptor)
 			w_cn = 0;
 			if (resized_img.at<uchar>(i, j) < 50)
 			{
+				ff++;
 				for (int l = i - 1; l < i + 1; ++l)
 				{
 					if (l < 0)
@@ -186,11 +523,81 @@ int getDescriptorFromImage(char* imageFile, float* descriptor)
 			d_ptr++;
 		}
 	}
-
+	descriptor[d_ptr + 1] =(float)ff / (resized_img.rows * resized_img.cols);
 	return 0;
 }
 
-int _getDescriptorFromImage(char* imageFile, float* descriptor)
+int _zg2_getDescriptorFromImage(char* imageFile, float* descriptor)
+{
+	cv::Mat input_img = cv::imread(imageFile, 1);
+	if (!input_img.data) {
+		cout << "open image file:" << imageFile << " failed!" << endl;
+		return -1;
+	}
+
+	cv::Mat resized_img;
+	cv::Mat gray_img;
+	cv::Mat canny_img;
+
+	//// resizing image
+	cv::resize(input_img, resized_img, cv::Size(48, 48), 0.0, 0.0, 1);
+	//// resizing end
+
+
+	//// Generate canny images
+	cv::cvtColor(resized_img, gray_img, CV_BGR2GRAY);
+	cv::Canny(gray_img, canny_img, 50.0, 200.0, 3, true);
+	//// Generate canny end
+
+	////edge trace////
+
+	///// Initialize
+	int d_ptr, d_ptr02; // pointer for descriptor[ ]
+	for (d_ptr = 0; d_ptr < MAX_DIM_DESCRIPTOR; ++d_ptr)
+	{
+		descriptor[d_ptr] = 0;
+	}
+
+	d_ptr = 0;
+	d_ptr02 = 0;
+	///// Get trace
+	float cn = 0.0;
+	float trace = 0.0;
+
+	for (int i = 0; i < canny_img.cols; i++)
+	{
+		cn = 0;
+		for (int j = 0; j< canny_img.rows; j++)
+		{
+			if (canny_img.at<uchar>(i, j) >50)
+			{
+				cn++;
+			}
+		}
+		trace = cn;
+		descriptor[d_ptr] = trace;
+		//cout << "descriptor[ " << d_ptr <<"] = "<<descriptor[d_ptr]<< endl;
+		d_ptr++;
+	}
+	for (int j = 0; j< canny_img.rows; j++)
+	{
+		cn = 0;
+		for (int i = 0; i < canny_img.cols; i++)
+		{
+			if (canny_img.at<uchar>(i, j) >50)
+			{
+				cn++;
+			}
+		}
+		trace = cn;
+		descriptor[d_ptr] = trace;
+		//cout << "descriptor[ " << d_ptr <<"] = "<<descriptor[d_ptr]<< endl;
+		d_ptr++;
+	}
+
+	return 0;
+}
+int _33_getDescriptorFromImage(char* imageFile, float* descriptor)
 {
 	cv::Mat input_img = cv::imread(imageFile, 1);
 	if (!input_img.data) {
@@ -260,7 +667,7 @@ int _getDescriptorFromImage(char* imageFile, float* descriptor)
 	// ****"i < HULL_LIST_MAX" in for statement is add by wtq, 
 	// ****to avoid illegal visitation to convexhull_list_x(y) 
 	// *******************************************************************
-	for (int i = 0; i < contours.size() && i < HULL_LIST_MAX; ++i)
+	for (int i = 0; i < contours.size() && i < HULL_LIST_MAX-1; ++i)
 	{
 		current_k = 0;
 
@@ -378,6 +785,8 @@ int _getDescriptorFromImage(char* imageFile, float* descriptor)
 					xq = (int)((1.0 - (float)k / length_ij)*xi + (float)k / length_ij*xj);
 					yq = (int)((1.0 - (float)k / length_ij)*yi + (float)k / length_ij*yj);
 
+					if (yq < 1 || xq < 1)
+						continue;
 					///// Check exceptional intersection for input_img
 
 					count_edge = 0;
@@ -510,9 +919,12 @@ void readFiles(int classLoc, Cfname *imgFiles, float *labels)
 
 
 void init(){
+	// init Labels
 	for (int i = 0; i < CLASS_NUM; i++){
 		classLabels[i] = (float)i;
 	}
+	// Get Boxes
+	//getTextBox("dirty.jpg");
 }
 int readTrainFiles(Cfname *trainFiles, int trainClassCount[], int trainFilesCount, float *labels)
 {
@@ -528,12 +940,13 @@ int readTrainFiles(Cfname *trainFiles, int trainClassCount[], int trainFilesCoun
 int readTestFiles(Cfname *testFiles, int testFilesCount)
 {
 	readFiles(TEST_LOC, testFiles, NULL);
-
+	/*
 	for (int i = 0; i < testFilesCount; i++){
 		cout << testFiles[i] << endl;
-	}
+	}*/
 	return testFilesCount;
 }
+
 int main(int argc, char **argv)
 {
 	init();
@@ -541,6 +954,7 @@ int main(int argc, char **argv)
 	int classDirs[CLASS_NUM];
 	int testFilesCount = getClassFilesNum(testDir);
 	int trainFilesCount = getTrainFilesNum(classDirs);
+
 
 	Cfname *imgFiles = new Cfname[trainFilesCount];
 	Cfname *testFiles = new Cfname[testFilesCount];
@@ -558,8 +972,9 @@ int main(int argc, char **argv)
 
 
 	///// 提取训练用图片descriptor
-
+	cout << "***********Get descriptor for train...***********" << endl;
 	for (int i = 0; i < rows_for_train; i++) {
+		cout << i << "/" << rows_for_train << endl;
 		try{
 
 			getDescriptorFromImage(imgFiles[i], descriptors[i]);
@@ -572,7 +987,9 @@ int main(int argc, char **argv)
 
 
 	///// 提取测试用图片descriptor
+	cout << "***********Get descriptor for test....***********" << endl;
 	for (int i = 0; i < rows_for_test; i++) {
+		cout << i << "/" << rows_for_test << endl;
 		string fn = testFiles[i];
 		getDescriptorFromImage(testFiles[i], descriptorsTest[i]);
 	}
@@ -587,6 +1004,7 @@ int main(int argc, char **argv)
 	}
 
 	///// 初始化SVM参数
+	cout << "Initial SVM..." << endl;
 	CvSVMParams params;
 	params.svm_type = CvSVM::C_SVC;
 	params.kernel_type = CvSVM::LINEAR;
@@ -597,30 +1015,35 @@ int main(int argc, char **argv)
 	for (int i = 0; i < rows_for_train; i++)
 		for (int j = 0; j < MAX_DIM_DESCRIPTOR; j++)
 			if (descriptors[i][j] != 0)
-				;//cout << "des_for_train[" << i << "][" << j << "]" << descriptors[i][j] << endl;
+				 cout << "des_for_train[" << i << "][" << j << "]" << descriptors[i][j] << endl;
+	//system("pause");
+	cout << endl << "Training..." << endl;
 	SVM.train(trainDataMat, labelsMat, Mat(), Mat(), params);
 	///// 使用默认参数的SVM进行训练
 	//SVM.train(trainDataMat, labelsMat);
 
 
 	float response; //预测结果
-	float right = 0; //预测正确的数量，用于计算正确率
 
 	///// 根据每张图片的descriptor生成测试用数据，并进行测试
-
+	cout << "Response of SVM:" << endl;
 	Mat testDesMat(1, MAX_DIM_DESCRIPTOR, CV_32FC1);
 	for (int i = 0; i < rows_for_test; i++) {
 		for (int j = 0; j < MAX_DIM_DESCRIPTOR; j++) {
 			///// 生成测试数据
 			testDesMat.at<float>(0, j) = descriptorsTest[i][j];
+			if (0 != descriptorsTest[i][j])
+				;// cout << "des_for_test[" << j << "] = " << testDesMat.at<float>(0, j) << ":" << descriptorsTest[i][j] << endl;
 		}
 		///// 进行预测
 		response = SVM.predict(testDesMat);
-		std::cout << " RESULT" << i << "---" << response << "---" << testFiles[i] << endl;
+	//if (response = 1)
+			//std::cout << " RESULT" << i << "---" << response << "---" << testFiles[i] << endl;
 	}
 
 
 	///// kmeans
+	cout << "Response of K-Means:" << endl;
 	Mat testKmeansData(rows_for_test, MAX_DIM_DESCRIPTOR, CV_32FC1);
 	Mat kmeansLabels;
 	for (int i = 0; i < rows_for_test; i++)
@@ -630,8 +1053,16 @@ int main(int argc, char **argv)
 		}
 	cv::kmeans(testKmeansData, 2, kmeansLabels, TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 20, 1.0), 10, KMEANS_PP_CENTERS);
 	///// result of kmeans:
+	int count = 0;
 	for (int i = 0; i < rows_for_test; i++)
+		if (0 != kmeansLabels.at<int>(i))
+		{
+		count++;
 		cout << "K-Means--" << testFiles[i] << ":" << kmeansLabels.at<int>(i) << endl;
+		}
+	cout << "Result : " << count << endl;
+	waitKey(0);
+
 	system("pause");
 
 	return 0;
